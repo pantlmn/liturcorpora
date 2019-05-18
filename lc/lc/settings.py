11e92a14dbde7b7d3b712df97b7db0e888e3f90c
Panteleimon <pantlmn@gmail.com>
2019-05-18 15:43:28 +0300

    fix url + root

--- lc/lc/settings.py
+++ lc/lc/settings.py
@@ -118,6 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-STATIC_URL = os.path.join(BASE_DIR, "static_local/")
-STATIC_ROOT = os.path.join(BASE_DIR, "static_local/")
++STATIC_URL = '/static/'
++STATIC_ROOT = '/webapps/test/lc/static_local/'
