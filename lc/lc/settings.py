b1486610fd76abfd56262e0919aa8e86098a5ecc
Panteleimon <pantlmn@gmail.com>
2019-05-18 16:03:54 +0300

    remove trash

--- lc/lc/settings.py
+++ lc/lc/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-+STATIC_URL = '/static/'
-+STATIC_ROOT = '/webapps/test/lc/static_local/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/webapps/test/lc/static_local/'
