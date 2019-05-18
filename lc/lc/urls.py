2595e594222cf6863b1bffbcf549bc88d4d6319b
Panteleimon <pantlmn@gmail.com>
2019-05-18 16:14:26 +0300

    + общий индекс

--- lc/lc/urls.py
+++ lc/lc/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('rc_dic.urls')),
     path('ruscorpora/', include('rc_dic.urls')),
     path('admin/', admin.site.urls),
 ]
