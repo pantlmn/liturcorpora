af2a72a29dd87f930887efec302cf386409af7d6
Panteleimon <pantlmn@gmail.com>
2019-06-02 22:20:17 +0300

    +robots.txt

--- lc/rc_dic/urls.py
+++ lc/rc_dic/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'rc_dic'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('robots.txt', views.robots, name='robots'),
     path('ruscorpora/', views.index, name='index'),
     path('ruscorpora/lemmas/', views.list_lemmas, name='list_lemmas'),
     path('ruscorpora/lemmas/<int:lemma_id>/', views.lemma_info, name='lemma_info'),
