1ac2050ac23f8f32e6e4e7560c69c654d00df206
Panteleimon <pantlmn@gmail.com>
2019-08-11 13:12:06 +0300

    move files

--- lc/ponomar/models.py
+++ lc/ponomar/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import sqlite3
+import pandas
+import re
+from tqdm import tqdm
 
-# Create your models here.
+from rc_dic.models import Language
+
+class Book(models.Model):
+    """Богослужебные книги"""
+    name  = models.CharField(max_length=12, unique=True)
+    name_long   = models.CharField(max_length=45, null=True, default=None)
+    language       = models.ForeignKey(Language, on_delete=models.CASCADE, null=True, default=None)
+
+    def __str__(self):
+        return self.name_long + " (" + self.language + ")"
