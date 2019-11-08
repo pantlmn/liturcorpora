df42fbcb119a21b1bc194263ee6137ccb328af37
Panteleimon <pantlmn@gmail.com>
2019-11-09 02:45:32 +0300

    Импорт книжек ponomar

--- lc/lc/settings.py
+++ lc/lc/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'rc_dic.apps.RC_DicConfig',
+    'ponomar.apps.PonomarConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- lc/ponomar/models.py
+++ lc/ponomar/models.py
@@ -3,14 +3,37 @@ import sqlite3
 import pandas
 import re
 from tqdm import tqdm
+import os
+from bs4 import BeautifulSoup
 
 from rc_dic.models import Language
 
 class Book(models.Model):
     """Богослужебные книги"""
-    name  = models.CharField(max_length=12, unique=True)
-    name_long   = models.CharField(max_length=45, null=True, default=None)
-    language       = models.ForeignKey(Language, on_delete=models.CASCADE, null=True, default=None)
+    name        = models.CharField(max_length=40, unique=True, default=None)
+    name_long   = models.CharField(max_length=128, null=True, default=None)
+    language    = models.ForeignKey(Language, on_delete=models.CASCADE, null=True, default=None)
 
     def __str__(self):
         return self.name_long + " (" + self.language + ")"
+
+
+def ponomar_import_all_books():
+    print ("Составляем список книг в репозитории…")
+    Book.objects.all().delete()
+    lang = Language.objects.get(name='csl_utf')
+    path = "../source_data/ponomar/"
+    books = []
+    for dirname in os.listdir(path):
+        if (not dirname.startswith('.')) and os.path.isdir(path + dirname):
+            # print (path + dirname)
+            manifest = path + dirname + '/manifest.xml'
+            if os.path.exists(manifest):
+                with open(manifest, 'r') as f:
+                    soup = BeautifulSoup(f, "html.parser")
+                    title = soup.find('meta', attrs = {'name' : 'title'})
+                    if title:
+                        books += [Book(name = dirname, name_long = title['content'], language = lang)]
+    print ("Добавляем %d книг." % len(books), end = ' ')
+    Book.objects.bulk_create(books)
+    print ("Готово.")
